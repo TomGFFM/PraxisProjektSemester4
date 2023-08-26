be0816063937949214e478c1329202907776ec65
TomGFFM <thomas.graf@icloud.com>
2023-08-27 00:47:49 +0200

    - changed folder structure due to poetry requirements - added mock for automated input - added all files relevant for model training

--- MiniMetroBot/core/__init__.py
+++
@@ -1,1 +0,0 @@
-from .ScreenDetector import ScreenDetector
--- MiniMetroBot/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from pkg_resources import resource_filename
-from ultralytics import YOLO
-
-best_model = YOLO(resource_filename("MiniMetroBot", "models/best.pt"))
